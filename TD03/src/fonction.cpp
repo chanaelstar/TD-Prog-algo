e7a54b258836aa469263b18fb2b4b90bc0bae92e
chanaelstar <chanaelstar@gmail.com>
2025-03-18 10:34:50 +0100

    Début TD03

---
+++ TD03/src/fonction.cpp
@@ -0,0 +1,11 @@
+#include "fonction.hpp"
+
+
+bool is_sorted(std::vector<int> const& vec){ 
+    return std::is_sorted(vec.begin(), vec.end()); }
+
+
+// Exercice 1
+void bubble_sort(std::vector<int> & vec){
+    
+}
---
+++ TD03/src/fonction.hpp
@@ -0,0 +1,9 @@
+#pragma once
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+bool is_sorted(std::vector<int> const& vec);
+
+// Exercice 1 
+void bubble_sort(std::vector<int> & vec);
