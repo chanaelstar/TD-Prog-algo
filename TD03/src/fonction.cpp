f2e036c6a1f7c127ba549a46814975ad5a0469fc
chanaelstar <chanaelstar@gmail.com>
2025-03-18 17:07:21 +0100

    exo1

--- TD03/src/fonction.cpp
+++ TD03/src/fonction.cpp
@@ -7,5 +7,16 @@ bool is_sorted(std::vector<int> const& vec){
 
 // Exercice 1
 void bubble_sort(std::vector<int> & vec){
-    
+    for (int i = 0; i < vec.size(); i++){
+        for (int j = 0; j < vec.size()-1; j++){
+            if (vec[j] > vec[j+1]){
+                std::swap(vec[j], vec[j+1]);
+            }
+        }
+    }
+}
+
+// Exercice 2
+void merge_sort(std::vector<float> & vec) {
+    merge_sort(vec, 0, vec.size()-1);
 }
--- TD03/src/fonction.hpp
+++ TD03/src/fonction.hpp
@@ -2,8 +2,14 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <string>
+
 
 bool is_sorted(std::vector<int> const& vec);
 
 // Exercice 1 
 void bubble_sort(std::vector<int> & vec);
+
+// Exercice 2
+void merge_sort_merge(std::vector<float> & vec, size_t const left, size_t const middle, size_t const right);
+void merge_sort(std::vector<float> & vec, size_t const left, size_t const right);
